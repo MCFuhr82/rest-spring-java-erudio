f7914d77a715d27d0befa16dd9e87b965e44170e
MCFuhr82 <marcelinhofuhr@gmail.com>
2023-01-30 15:52:04 -0300

    First commit

---
+++ src/test/java/com/blinkdevops/restspringjava/StartupTests.java
@@ -0,0 +1,13 @@
+package com.blinkdevops.restspringjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
